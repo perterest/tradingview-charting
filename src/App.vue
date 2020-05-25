1382f5d3480133b9bfcab4247b5790efd2366ead
perterest <qingfengpei@gmail.com>
2020-05-25 15:42:35 +0800

    initial code

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <TVChartContainer :symbol="'BTCUSDT'" :interval="'60'"></TVChartContainer>
+</template>
+
+<script>
+  import TVChartContainer from './components/TVChartContainer.vue'
+  export default {
+    name: 'app',
+    components: {
+      TVChartContainer
+    }
+  }
+</script>
